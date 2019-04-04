0d2e0923d81a5897194e71ae8e3a7fb162532156
Tiago Loriato <tloriatosimoes@gmail.com>
2019-04-03 23:36:21 -0300

    Adding MySQLDb as another Container

--- explorer/runexplorer.sh
+++ explorer/runexplorer.sh
@@ -7,25 +7,22 @@ echo "Start the chain"
 multichaind -daemon -txindex=1 -shrinkdebugfilesize $CHAINNAME@$MASTER_NODE:$NETWORK_PORT -autosubscribe=streams
 
 echo "Sleep for 10 seconds so the slave node has initialised"
-sleep 10
+sleep 10 
 
 echo "Setup /root/.multichain/$CHAINNAME/multichain.conf"
 echo "rpcport=$RPC_PORT" >> /root/.multichain/$CHAINNAME/multichain.conf
 
 echo "Setup /root/explorer.conf"
 cat << EOF > /root/explorer.conf
-port 2750
-host 0.0.0.0
 datadir += [{
         "dirname": "~/.multichain/$CHAINNAME",
         "loader": "default",
         "chain": "MultiChain $CHAINNAME",
         "policy": "MultiChain"
         }]
-dbtype = sqlite3
-connect-args = dockerchain.explorer.sqlite
+dbtype = MySQLdb
+connect-args {"user":"abe","db":"abe","passwd":"PASSWORD","host":"mysqldb","port":3306}
 EOF
 
 echo "Run the explorer"
-python -m Mce.abe --config /root/explorer.conf --commit-bytes 100000 --no-serve  --recent_tx_interval_ms=2000 --catch_up_tx_interval_secs=10
-python -m Mce.abe --config /root/explorer.conf --recent_tx_interval_ms=2000 --catch_up_tx_interval_secs=10
+python -m Mce.abe --config /root/explorer.conf --commit-bytes 100000 &
