1ee4fa81fcdc5e876cbf974ce0d61a2cfc8da341
Tiago Loriato <tloriatosimoes@gmail.com>
2019-04-04 11:02:04 -0300

    A little bit prettier

--- explorer/worker.sh
+++ explorer/worker.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-if [ ! -f /root/explorer.conf ]; then
-    echo "Setting up the explorer"
-    /root/setupexplorer.sh
-fi
-
-echo "Running the explorer"
-ls
 python -m Mce.abe --config /root/explorer.conf
