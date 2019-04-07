fd9f6c5a7a5685eaa2856dc1b6f0f38e3a37d1ee
FranciscoThiesen <franciscogthiesen@gmail.com>
2019-04-07 20:42:19 -0300

    Fixes #2

--- explorer/setupexplorer.sh
+++ explorer/setupexplorer.sh
@@ -7,15 +7,15 @@ echo "Start the chain"
 multichaind -daemon -txindex=1 -shrinkdebugfilesize $CHAINNAME@$MASTER_NODE:$NETWORK_PORT -autosubscribe=streams
 
 echo "Sleep for 10 seconds so the slave node has initialised"
-sleep 10 
+sleep 10
 
-echo "Setup .multichain/$CHAINNAME/multichain.conf"
-echo "rpcport=$RPC_PORT" >> /.multichain/$CHAINNAME/multichain.conf
+echo "Setup /root/.multichain/$CHAINNAME/multichain.conf"
+echo "rpcport=$RPC_PORT" >> /root/.multichain/$CHAINNAME/multichain.conf
 
 echo "Setup /root/explorer.conf"
 cat << EOF > /root/explorer.conf
 datadir += [{
-        "dirname": "/.multichain/$CHAINNAME",
+        "dirname": "~/.multichain/$CHAINNAME",
         "loader": "default",
         "chain": "MultiChain $CHAINNAME",
         "policy": "MultiChain"
@@ -24,3 +24,5 @@ dbtype = MySQLdb
 connect-args {"user":"abe","db":"abe","passwd":"PASSWORD","host":"mysqldb","port":3306}
 EOF
 
+python -m Mce.abe --config /root/explorer.conf --commit-bytes 100000 --no-serve  --recent_tx_interval_ms=2000 --catch_up_tx_interval_secs=10
+python -m Mce.abe --config /root/explorer.conf --recent_tx_interval_ms=2000 --catch_up_tx_interval_secs=10
